fe260af49a1dd69ffd4c534b5257d5d90d1a695f
Gui Parpineli <guiparpineli@GuiPC>
2023-01-18 23:20:38 -0300

    add initial files

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
 func main() {
 
+	r := gin.Default()
+	public := r.Group("/")
+	public.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "Hello World"})
+	})
+
+	r.Run()
 }
